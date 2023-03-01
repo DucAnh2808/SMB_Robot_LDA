5297efbfcfd329dedacbbcf1f25fbcd8c89e3aab
DucAnh2808 <leanht1k59@gmail.com>
2023-03-01 14:16:00 +0700

    a

---
+++ wsdrone/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ducanh/wsdrone/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/ducanh/wsdrone/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/ducanh/wsdrone/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/ducanh/wsdrone/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ducanh/wsdrone/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ducanh/wsdrone/src:$ROS_PACKAGE_PATH"
